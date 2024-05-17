c95eb6ee86fcd1d56367d7aca3aaf020772ae1bd
Ferryal Fajar <ferryal21@gmail.com>
2024-05-17 10:24:05 +0700

    [feat] add main layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-layout view="lHr lpR lFr">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <AppFooter />
+  </q-layout>
+</template>
+
+<script setup>
+import AppFooter from "components/AppFooter.vue";
+</script>
